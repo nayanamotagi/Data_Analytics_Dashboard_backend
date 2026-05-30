0f6113c42109e8fc93d1f2ea6683574b23f44783
nayanamotagi <nayanamotagi24@gmail.com>
2026-05-30 10:00:31 +0530

    fix the cors not connected error

--- server.js
+++ server.js
@@ -19,20 +19,8 @@ app.use((req, res, next) => {
   next();
 });
 
-const allowedOrigins = [
-  'https://data-analytics-dashboard-frontend-4ooydz6r7.vercel.app',
-  'https://data-analytics-dashboard-frontend-optlcqoj8.vercel.app',
-  'https://data-analytics-dashboard-frontend-1h16fvg9c.vercel.app',
-];
-
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
